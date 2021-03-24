722a37b5031a489c3e22f6dcf198f64fbfc13e7c
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-03-24 16:52:01 -0300

    0.3

--- app.js
+++ app.js
@@ -1,48 +1,65 @@
+//carregando modulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require("body-parser")
+const { getPriority } = require('node:os')
+const app = express()
+//const mongoose = require ('mongoose')
+//config
+
+//rotas
+//
+//outros
+const PORT= 8081
+app.listen(PORT,()=>{
+console.log("servidor rodando na porta:"+PORT)
+})
+
 const mongoose = require("mongoose")
 mongoose.Promise= global.Promise
 
 mongoose.connect('mongodb://localhost:27017/teste', {useNewUrlParser: true}).then(()=>{
-    console.log("conectado com Sucesso ")
+console.log("conectado com Sucesso ")
 }).catch((error)=>{
-    console.log("deu erro ")
+console.log("deu erro ")
 })
 
 //model 
 
 //definindo o model
 
-    const usuarioSchema = mongoose.Schema({
-        nome:{
-            type: String,
-            require:true
-        },
-        sobrenome:{
-            type: String,
-            require:true
-            
-        },
-        email:{
-            type:String,
-            require:true
-        },
-        idade:{
-            type :Number,
-            require:true
-        }
+const usuarioSchema = mongoose.Schema({
+    nome:{
+        type: String,
+        require:true
+    },
+    sobrenome:{
+        type: String,
+        require:true
         
-    })
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    idade:{
+        type :Number,
+        require:true
+    }
+    
+})
 //collection
-    mongoose.model('usuarios', usuarioSchema)
+mongoose.model('usuarios', usuarioSchema)
 
-    const zenilton = mongoose.model('usuarios')
+const zenilton = mongoose.model('usuarios')
 
-    new zenilton({
-        nome: "zenilton",
-        sobrenome:"sousa",
-        email:"ze18.fv@gmail.com",
-        idade:23
-    }).save().then(()=>{
-        console.log("cadastrado com sucesso ")
-    }).catch(()=>{
-        console.log("ero ao cadastrar")
-    })
+new zenilton({
+    nome: "zenilton",
+    sobrenome:"sousa",
+    email:"ze18.fv@gmail.com",
+    idade:23
+}).save().then(()=>{
+    console.log("cadastrado com sucesso ")
+}).catch(()=>{
+    console.log("ero ao cadastrar")
+})
