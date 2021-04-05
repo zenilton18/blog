acab5946d33fc3b483d264859d68d08c274a4518
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-04-05 14:33:29 -0300

    0.6

--- routes/admin.js
+++ routes/admin.js
@@ -20,18 +20,41 @@ router.get('/',(req, res)=>{
 
  })
  router.post("/categorias/nova", (req,res)=>{
-  
-     const novaCategoria={
-        nome: req.body.nome,
-        slug: req.body.slug
 
+    var erros=[]
+
+    if(!req.body.nome ||typeof req.body.nome == undefined || req.body.slug==null){
+
+        erros.push({texto:"nome invalido "})
     }
-    new Categoria(novaCategoria).save().then(()=>{
-      console.log("salvo ")
-    }).catch((erro)=>{
-        console.log("erro"+erro)
-    })
- 
+
+    if(!req.body.slug ||typeof req.body.slug == undefined || req.body.slug==null){
+
+        erros.push({texto:"slug invalido "})
+    }
+    if(req.body.nome.length <2){
+        erros.push({texto:"nome pequeno "})
+    }
+     if (erros.length >0){
+         res.render("admin/addcategoria",{erros: erros })
+     }
+     else{
+        const novaCategoria={
+            nome: req.body.nome,
+            slug: req.body.slug
+    
+        }
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash("success_msg","Mensagem salva com Sucesso ")
+          res.redirect("/admin/categorias")
+        }).catch((erro)=>{
+            req.flash("error_msg","Erro ao Salvar,Tente novamente mais tarde !")
+
+            res.redirect("/admin")
+        })
+
+     }
+  
 
   })
 module.exports = router 
