d753b135ea0ab9b3e995a72eb1b43f82c1553e3e
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-04-14 22:15:33 -0300

    00

--- routes/admin.js
+++ routes/admin.js
@@ -66,15 +66,21 @@ router.get('/',(req, res)=>{
 
   })
 
-  router.get("/categorias/edit/:id",(req,res)=>{
-      Categoria.findOne({_id: req.params.id}).lean().then((categoria)=>{
-        res.render("admin/editcategorias", {categoria:categoria})
-      }).catch((erro)=>{
-        req.flash("error_msg","essa categoria nao existe!")
+  
 
-        res.redirect("/admin/categorias")
+    router.get("/categorias/edit/:id",(req,res)=>{
+        Categoria.findOne({_id : req.parms.id}).lean().then((categoria)=>{
+        res.render("admin/editcategorias", {categoria:categoria})  
+        }).catch((erro)=>{
+            req.flash("error_msg","essa categoria nao existe!")
+
+            res.redirect("/admin/categorias")
+        })  
+        
     })
       
 
   })
+
+  
 module.exports = router 
