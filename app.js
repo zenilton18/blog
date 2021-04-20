0ea7d895abbb7fb9b2aee27d8bc760c1299b01ce
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-04-19 23:02:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
 //carregando modulos
-
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require("body-parser")
+    const admin = require("./routes/admin")
+    const path= require ("path")
+    const app = express()
+    const mongoose = require ('mongoose')
+    const session = require ("express-session")
+    const flash = require ("connect-flash")
+//config  
     //body-parser
     //sessão
     app.use(session({
