a043cc5359809b051a3dc4e1e6292a8f876d34fd
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-04-23 20:52:01 -0300

    ok
    teste

---
+++ models/Postagem.js
@@ -0,0 +1,36 @@
+const { Mongoose } = require("mongoose");
+const mongose = require ("mongose") 
+const Schema = mongose.Shema;
+
+const Postagem = new Schema ({
+
+    titulo:{
+        type: String,
+        required:true
+    },
+    slug:{
+        type:String,
+        required: true
+    },
+    descricao:{
+        type:String,
+        required:true
+    
+    },
+    conteudo:{
+        type:String,
+        required:true
+    },
+    categoria:{
+        type: Schema.Types.objectId,
+        ref:"categorias",
+        required:true
+    },
+     data:{
+         type:Date,
+         default: Date.now()
+     }
+
+    
+})
+Mongoose.model("postagens", Postagem)
