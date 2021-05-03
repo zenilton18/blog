b4aafea65d5d3392cb30a3c748b0e52f6fe2cd94
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-05-03 20:40:52 -0300

    Update Postagem.js

--- models/Postagem.js
+++ models/Postagem.js
@@ -1,29 +1,7 @@
 const mongoose = require ("mongoose");
 const Schema= mongoose.Schema;
 
-const Postagem = new Schema({
-    titulo:{
-        type: String,
-        required:true
-    },
-    slug:{
-        type:String,
-        required: true
-    },
-    descricao:{
-        type:String,
-        required:true
-    
-    },
-    conteudo:{
-        type:String,
-        required:true
-    },
-    categoria:{
-        type: Schema.Types.ObjectId,
-        ref:"categorias",
-        required:true
-    },
+const Po
      data:{
          type:Date,
          default: Date.now()
