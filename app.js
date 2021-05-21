f7e33b7f8ac70d5611454833981ffd1961b61a99
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-05-20 22:25:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,22 +24,6 @@
         res.locals.error_msg = req.flash("error_msg")
         next()
     })
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.use(bodyParser.json())
-    //handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
-    //mongoose
-    mongoose.Promise= global.Promise;
-    mongoose.connect("mongodb://localhost/blogapp",{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-        console.log("conectado ao mongo ")
-    }).catch((erro)=>{
-        console.log("erro "+erro)
-    })
-//public
-    app.use(express.static(path.join(__dirname,"public")))
-//rotas
-    app.use('/admin', admin)
 //outros
 const PORT= 8081
 app.listen(PORT,()=>{
