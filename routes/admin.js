69a76282d633b35d8bdb16a01dad7901adabb342
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-06-02 18:24:43 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,5 @@
 const express = require("express")
-const router = express.Router()
-const mongoose= require("mongoose")
-require("../models/Categoria")
-const Categoria= mongoose.model("categorias")
-require("../models/Postagem")
-const Postagem =mongoose.model("postagens")
-
-
-
-
-
-    router.get('/',(req, res)=>{
-        res.render('admin/index')
-    })
-
-  router.get('/post', (req,res)=>{
-      res.render("admin/index")
-  })
-
-  router.get('/categorias', (req,res)=>{
-    Categoria.find().lean().sort({date:'desc'}).then((categorias)=>{
-        res.render("admin/categorias" , {categorias: categorias})
+c
 
     }).catch((erro)=>{
             req.flash("error_msg" , "erro ao listar categorias ")
