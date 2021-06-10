e37530c708c0f08a6c5e4b757c894acf81dcf694
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-06-09 22:32:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,41 +8,4 @@
     const app = express()
     const mongoose = require ('mongoose')
     const session = require ("express-session")
-    const flash = require ("connect-flash")
-//config  
-    //body-parser
-    //sessão
-    app.use(session({
-        secret:"cursonode",
-        resave:true,
-        saveUninitialized:true
-    }))
-    app.use(flash())
-    //middleware
-    app.use((req,res,next)=>{
-        res.locals.success_msg =req.flash("success_msg")
-        res.locals.error_msg = req.flash("error_msg")
-        next()
-    })
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.use(bodyParser.json())
-    //handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
-    //mongoose
-    mongoose.Promise= global.Promise;
-    mongoose.connect("mongodb://localhost/blogapp",{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-        console.log("conectado ao mongo ")
-    }).catch((erro)=>{
-        console.log("erro "+erro)
-    })
-//public
-    app.use(express.static(path.join(__dirname,"public")))
-//rotas
-    app.use('/admin', admin)
-//outros
-const PORT= 8081
-app.listen(PORT,()=>{
-    console.log("servidor rodando na porta:"+ PORT)
-})
-
+    co
