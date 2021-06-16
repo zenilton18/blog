a4e892abb9a28876b582000d2c986862a7894a3b
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-06-15 23:00:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,22 @@
     const admin = require("./routes/admin")
     const path= require ("path")
     const app = express()
-    const mongoose = require ('mo
+    const mongoose = require ('mongoose')
+    const session = require ("express-session")
+    const flash = require ("connect-flash")
+//config  
+    //body-parser
+    //sessão
+    app.use(session({
+        secret:"cursonode",
+        resave:true,
+        saveUninitialized:true
+    }))
+    app.use(flash())
+    //middleware
+    app.use((req,res,next)=>{
+        res.locals.success_msg =req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
         next()
     })
     app.use(bodyParser.urlencoded({extended:true}))
