e2f3cfa493a8d67c21be407642cf300a7ca31af6
Zenilton Sousa Silva <70024778+zenilton18@users.noreply.github.com>
2021-06-17 22:29:55 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -108,6 +108,66 @@ const Postagem =mongoose.model("postagens")
         res.redirect("/admin/categorias")
     }).catch((erro)=>{
         req.flash("error_msg","erro ao deletar ")
+        res.redirect("/admin/categorias")
+
+        })
+    })
+    router.get("/postagens",(req,res)=>{
+        Postagem.find().lean().populate("categoria").sort({date:"desc"}).then((postagens)=>{
+            res.render("admin/postagens",{postagens:postagens})
+        }).catch((erro)=>{
+            req.flash("error_msg","erro ao listar postagens")
+            res.render("admin/postagens")
+        })  
+        
+    })
+    
+
+
+    router.get("/postagens/add",(req,res)=>{
+        Categoria.find().lean().then((categorias)=>{
+            res.render("admin/addpostagem",{categorias:categorias})
+        }).catch((erro)=>{
+            req.flash("error_msg", "erro ao carregar formulario")
+            res.redirect("/admin")
+            
+        })
+
+    })
+    router.post("/postagens/nova",(req,res)=>{
+
+
+       var  erros= []
+       if(req.body.categoria== "0"){
+           erros.push({texto: "registre 1 categoria"})
+       }
+       if(erros.length > 0){
+        res.render("admin/addpostagem",{erros:erros})
+
+        }else{
+         const novaPostagem={ 
+         titulo: req.body.titulo,
+         descricao:req.body.descricao,
+         conteudo :req.body.conteudo,
+         categoria:req.body.categorias,
+         slug:req.body.slug
+     
+         }
+         new Postagem(novaPostagem).save().then(()=>{
+             req.flash("success_msg","Mensagem salva com Sucesso ")
+         res.redirect("/admin/postagens")
+         }).catch((erro)=>{
+             req.flash("error_msg","Erro ao Salvar,Tente novamente mais tarde !")
+
+             res.redirect("/admin/postagens")
+         })
+            }
+    })
+    
+    router.get("/postagens/edit/:id",(req,res)=>{
+        Postagem.findOne({id: req.params.body}).lean().then((postagem)=>{
+            Categoria.find().lean().then((categorias)=>{
+                res.render("admin/editpostagens", {categorias:categorias, postagem:postagem})
 
             }).catch((error)=>{
                 req.flash("error_msg","erro ao encontar categoria ")
